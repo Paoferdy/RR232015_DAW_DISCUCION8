5c8f349347fa7f905ffe8030a54541ca35df8b23
Paoferdy <118046534+Paoferdy@users.noreply.github.com>
2023-10-09 23:39:44 -0600

    Discución 08

---
+++ JS/localStorage.js
@@ -0,0 +1,29 @@
+const guardarDatosButton = document.getElementById('guardar');
+
+guardarDatosButton.addEventListener('click', function() {
+    const nombre = document.getElementById('nombre').value;
+    const pais = document.getElementById('pais').value;
+    const biografia = document.getElementById('biografia').value;
+
+    const datos = {
+        nombre,
+        pais,
+        biografia
+    };
+    localStorage.setItem('datos', JSON.stringify(datos));
+});
+
+
+setTimeout(function(){
+    const nombre = document.getElementById('nombre').value;
+    const pais = document.getElementById('pais').value;
+    const biografia = document.getElementById('biografia').value;
+
+    const datos = {
+        nombre,
+        pais,
+        biografia
+    };
+    localStorage.setItem('datos', JSON.stringify(datos));
+
+}, 5000);
